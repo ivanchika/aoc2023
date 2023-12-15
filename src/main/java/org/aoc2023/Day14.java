c720b1d0132c5e77ae4fb882a6377e901be89d5c
ivanchika <ivanchika@gmail.com>
2023-12-15 17:51:16 +0100

    Day 14

--- src/main/java/org/aoc2023/Day14.java
+++ src/main/java/org/aoc2023/Day14.java
@@ -1,7 +1,8 @@
 package org.aoc2023;
 
-import java.util.*;
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.stream.IntStream;
 
 public class Day14 implements IDay {
@@ -12,46 +13,131 @@ public class Day14 implements IDay {
 
     @Override
     public void puzzleOne(String path) {
-        List<String> items = Arrays.stream(Utils.readString(path).split(",")).toList();
-        System.out.println(items.stream().map(Day14::calculateValue).reduce(Integer::sum).orElse(0));
+        char[][] stones = Utils.readChar2DArrayList(path);
+        System.out.println(
+                IntStream.range(0, stones.length).map(j -> {
+                            int rock = stones.length + 1;
+                            int result = 0;
+                            for (int i = 0; i < stones.length; i++) {
+                                if (stones[i][j] == 'O') result += --rock;
+                                if (stones[i][j] == '#') rock = stones.length - i;
+                            }
+                            return result;
+                        })
+                        .reduce(Integer::sum)
+                        .orElse(0));
     }
 
     @Override
     public void puzzleTwo(String path) {
-        List<String> items = Arrays.stream(Utils.readString(path).split(",")).toList();
-        Map<Integer, List<String>> map = new HashMap<>();
-        IntStream.rangeClosed(0, 255).forEach(n -> map.put(n, new ArrayList<>()));
-        items.forEach(command -> {
-            String lens = command.split("(=)|(-)")[0];
-            int value = calculateValue(lens);
-            if (command.contains("=")) {
-                String val = command.replaceAll("(=)|(-)", " ");
-                Optional<String> replace = map.get(value).stream().filter(s -> s.startsWith(lens)).findFirst();
-                if (replace.isPresent()) {
-                    map.get(value).set(map.get(value).indexOf(replace.get()), val);
+        Map<Integer, Long> hashes = new HashMap<>();
+        char[][] stones = Utils.readChar2DArrayList(path);
+//        printArray(stones);
+        long steps = 1000000000L;
+        boolean repeat = false;
+        for (long step = 1; step <= steps; step++) {
+            north(stones);
+            west(stones);
+            south(stones);
+            east(stones);
+            int hash = Arrays.deepHashCode(stones);
+            if (!repeat) {
+                if (hashes.containsKey(hash)) {
+                    long prev = hashes.get(hash);
+                    long diff = step - prev;
+                    long multi = (steps - prev) / diff;
+                    step = prev + (multi * diff);
+                    repeat = true;
                 } else {
-                    map.get(value).add(val);
+                    hashes.put(hash, step);
                 }
-            } else {
-                map.get(value).removeIf(s -> s.startsWith(lens));
             }
-        });
+        }
 
-        System.out.println(map.entrySet().stream().map(entry ->
-                IntStream.rangeClosed(1, entry.getValue().size())
-                        .map(slot -> (entry.getKey() + 1) * slot * Integer.parseInt(entry.getValue().get(slot - 1).split(" ")[1]))
-                        .reduce(Integer::sum).orElse(0)
-        ).reduce(Integer::sum));
+        System.out.println(
+                IntStream.range(0, stones.length).map(j ->
+                                IntStream.range(0, stones.length).map(i ->
+                                        stones[i][j] == 'O' ? stones.length - i : 0
+                                ).reduce(Integer::sum).orElse(0))
+                        .reduce(Integer::sum)
+                        .orElse(0));
     }
 
-    private static int calculateValue(String command) {
-        AtomicInteger value = new AtomicInteger();
-        command.chars().forEach(ch -> {
-            value.addAndGet(ch);
-            value.set(value.get() * 17);
-            value.set(value.get() % 256);
-        });
-        return value.get();
+    private void north(char[][] stones) {
+        for (int j = 0; j < stones.length; j++) {
+            int rock = -1;
+            for (int i = 0; i < stones.length; i++) {
+                if (stones[i][j] == 'O') {
+                    if (i > rock + 1) {
+                        stones[rock + 1][j] = 'O';
+                        stones[i][j] = '.';
+                    }
+                    rock++;
+                }
+                if (stones[i][j] == '#') {
+                    rock = i;
+                }
+            }
+        }
     }
 
+    private void west(char[][] stones) {
+        for (int i = 0; i < stones.length; i++) {
+            int rock = -1;
+            for (int j = 0; j < stones.length; j++) {
+                if (stones[i][j] == 'O') {
+                    if (j > rock + 1) {
+                        stones[i][rock + 1] = 'O';
+                        stones[i][j] = '.';
+                    }
+                    rock++;
+                }
+                if (stones[i][j] == '#') {
+                    rock = j;
+                }
+            }
+        }
+    }
+
+    private void south(char[][] stones) {
+        for (int j = 0; j < stones.length; j++) {
+            int rock = stones.length;
+            for (int i = stones.length - 1; i >= 0; i--) {
+                if (stones[i][j] == 'O') {
+                    if (i < rock - 1) {
+                        stones[rock - 1][j] = 'O';
+                        stones[i][j] = '.';
+                    }
+                    rock--;
+                }
+                if (stones[i][j] == '#') {
+                    rock = i;
+                }
+            }
+        }
+    }
+
+    private void east(char[][] stones) {
+        for (int i = 0; i < stones.length; i++) {
+            int rock = stones.length;
+            for (int j = stones.length - 1; j >= 0; j--) {
+                if (stones[i][j] == 'O') {
+                    if (j < rock - 1) {
+                        stones[i][rock - 1] = 'O';
+                        stones[i][j] = '.';
+                    }
+                    rock--;
+                }
+                if (stones[i][j] == '#') {
+                    rock = j;
+                }
+            }
+        }
+    }
+
+    private static void printArray(char[][] array) {
+        System.out.println("------------------------");
+        Arrays.stream(array).forEach(System.out::println);
+        System.out.println("------------------------");
+    }
 }
