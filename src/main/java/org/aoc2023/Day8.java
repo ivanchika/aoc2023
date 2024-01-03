2908406f08a662ca5922afa700fcc6ef397df4f0
ivanchika <ivanchika@gmail.com>
2024-01-03 15:55:10 +0100

    Day 8 Part 2

--- src/main/java/org/aoc2023/Day8.java
+++ src/main/java/org/aoc2023/Day8.java
@@ -7,7 +7,7 @@ import java.util.Map;
 public class Day8 implements IDay {
 
     public static void main(String[] args) {
-        new Day8().runOne();
+        new Day8().run();
     }
 
     @Override
@@ -33,7 +33,6 @@ public class Day8 implements IDay {
         System.out.println(steps);
     }
 
-
     static class WhereToGo {
         String name;
         String left;
@@ -57,7 +56,35 @@ public class Day8 implements IDay {
 
     @Override
     public void puzzleTwo(String path) {
-
+        List<String> input = Utils.readStringList(path);
+        char[] instructions = input.get(0).toCharArray();
+        Map<String, WhereToGo> directions = new HashMap<>();
+        for (int i = 2; i < input.size(); i++) {
+            directions.put(input.get(i).substring(0, 3), new WhereToGo(input.get(i).substring(0, 3), input.get(i).substring(7, 10), input.get(i).substring(12, 15)));
+        }
+        System.out.println(directions.entrySet().stream()
+                .filter(entry -> entry.getKey().endsWith("A")).map(Map.Entry::getValue)
+                .map(next -> {
+                    long steps = 0;
+                    int step = 0;
+                    do {
+                        char where = instructions[step];
+                        if (where == 'L') next = directions.get(next.left);
+                        if (where == 'R') next = directions.get(next.right);
+                        step++;
+                        steps++;
+                        if (step == instructions.length) step = 0;
+                    } while (!next.name.endsWith("Z"));
+                    return steps;
+                })
+                .sorted()
+                .reduce(1L, (a, b) -> (a * b) / gcd(a, b)));
     }
+
+    static long gcd(long a, long b) {
+        if (b == 0) return a;
+        return gcd(b, a % b);
+    }
+
 
 }
