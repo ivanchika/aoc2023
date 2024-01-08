b47c6aa726f99f0fa8429d7ec9a9ab130b98c02a
ivanchika <ivanchika@gmail.com>
2024-01-08 15:37:56 +0100

    Day 10 Part 2

--- src/main/java/org/aoc2023/Day10.java
+++ src/main/java/org/aoc2023/Day10.java
@@ -1,47 +1,31 @@
 package org.aoc2023;
 
-import java.util.stream.Stream;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 
 public class Day10 implements IDay {
 
     public static void main(String[] args) {
-        new Day10().runOne();
+        new Day10().run();
     }
+
+    List<List<int[]>> loops = new ArrayList<>();
+    List<int[]> maxLoop = new ArrayList<>();
 
     @Override
     public void puzzleOne(String path) {
-        final char[][] chars = Utils.readChar2DArrayList(path);
-        int sX = -1;
-        int sY = -1;
-        for (int i = 0; i < chars.length; i++) {
-            for (int j = 0; j < chars[0].length; j++) {
-                if (chars[i][j] == 'S') {
-                    sY = i;
-                    sX = j;
-                    break;
-                }
-                if (sX > -1) break;
-            }
-        }
-
-        System.out.println(Stream.of(
-                        (calculate(chars, sX - 1, sY, 'L') + 1) / 2,
-                        (calculate(chars, sX + 1, sY, 'R') + 1) / 2,
-                        (calculate(chars, sX, sY - 1, 'U') + 1) / 2,
-                        (calculate(chars, sX, sY + 1, 'D') + 1) / 2
-                )
-                .max(Integer::compareTo)
-                .orElse(0));
+        System.out.println((maxLoop.size() + 1) / 2);
     }
 
-    private int calculate(char[][] chars, int sX, int sY, char d) {
-        int dist = 0;
+    private void calculate(char[][] chars, int sX, int sY, char d) {
+        List<int[]> loop = new ArrayList<>();
         while (sX >= 0 && sX < chars[0].length && sY >= 0 && sY < chars.length && chars[sY][sX] != 'S' && chars[sY][sX] != '.'
                 && ((d == 'L' && "-FL".contains(chars[sY][sX] + ""))
                 || (d == 'R' && "-J7".contains(chars[sY][sX] + ""))
                 || (d == 'U' && "|F7".contains(chars[sY][sX] + ""))
                 || (d == 'D' && "|JL".contains(chars[sY][sX] + "")))) {
-            dist++;
+            loop.add(new int[]{sY, sX});
             switch (d) {
                 case 'L': {
                     switch (chars[sY][sX]) {
@@ -118,11 +102,50 @@ public class Day10 implements IDay {
                 break;
             }
         }
-        return dist;
+        loops.add(loop);
+    }
+
+    @Override
+    public void initData(String path) {
+        int sX = -1;
+        int sY = -1;
+        char[][] chars = Utils.readChar2DArrayList(path);
+        for (int i = 0; i < chars.length; i++) {
+            for (int j = 0; j < chars[0].length; j++) {
+                if (chars[i][j] == 'S') {
+                    sY = i;
+                    sX = j;
+                    break;
+                }
+                if (sX > -1) break;
+            }
+        }
+        calculate(chars, sX - 1, sY, 'L');
+        calculate(chars, sX + 1, sY, 'R');
+        calculate(chars, sX, sY - 1, 'U');
+        calculate(chars, sX, sY + 1, 'D');
+        maxLoop = loops.stream().max(Comparator.comparingInt(List::size)).orElse(new ArrayList<>());
     }
 
     @Override
     public void puzzleTwo(String path) {
+        // Pick's theorem
+        // area = interior + (boundaries / 2) - 1
+        int boundaries = maxLoop.size() + 1;
+        double interior = area(maxLoop) - (boundaries / 2.0) + 1;
+        System.out.println((int) interior);
+
     }
 
+    private static double area(List<int[]> loop) {
+        // Shoelace formula
+        // area = (x1*y2 - x2*y1 + x2*y3 - x3*y2 + .... + xn*y1 - x1*yn) / 2
+        int n = loop.size();
+        double area = 0.0;
+        for (int i = 0; i < n - 1; i++) {
+            area += loop.get(i)[1] * loop.get(i + 1)[0] - loop.get(i + 1)[1] * loop.get(i)[0];
+        }
+        area += loop.get(n - 1)[1] * loop.get(0)[0] - loop.get(0)[1] * loop.get(n - 1)[0];
+        return Math.abs(area) / 2.0;
+    }
 }
